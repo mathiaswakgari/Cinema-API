92b0183c75b09de776950a6fba2aedf533f42e89
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-07 13:44:24 +0300

    Express configuration and default routes intiation

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Cinema");
+});
+app.get("/api/movies", (req, res) => {
+  res.send([
+    { id: 1, title: "movie One" },
+    { id: 2, title: "movie Two" },
+    { id: 3, title: "movie Three" },
+  ]);
+});
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
