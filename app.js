2bc3cd568d1f113f0756e5a0abc0ed0a55a1320f
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-07 14:26:24 +0300

    defined movies array in memory.

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
 
+const movies = [
+  { id: 1, title: "movie One" },
+  { id: 2, title: "movie Two" },
+  { id: 3, title: "movie Three" },
+];
+
 app.get("/", (req, res) => {
   res.send("Welcome to Cinema");
 });
 app.get("/api/movies", (req, res) => {
-  res.send([
-    { id: 1, title: "movie One" },
-    { id: 2, title: "movie Two" },
-    { id: 3, title: "movie Three" },
-  ]);
+  res.send(movies);
 });
 app.get("/api/movies/:id", (req, res) => {
-  res.send(req.params);
+  const movie = movies.find((movie) => movie.id == req.params.id);
+  if (!movie) return res.status(404).send("Movie not found.");
+  return res.send(movie);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
