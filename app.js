c3b625dd271d796a985adf770e7233e429ca9065
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-09 11:39:45 +0300

    Connect to database.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const Joi = require("joi");
 const config = require("config");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
@@ -19,6 +19,19 @@ if (app.get("env") === "development") {
   console.log("Morgan enabled");
   app.use(morgan("tiny"));
 }
+
+// database
+
+mongoose
+  .connect("mongodb://127.0.0.1/cinema", {
+    useNewUrlParser: true,
+
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) =>
+    console.error("Failed connecting to MongoDB.", error.message)
+  );
 
 // Home Section
 app.use("/api", homeRoute);
