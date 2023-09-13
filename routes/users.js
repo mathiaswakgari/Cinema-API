81ae2e3bb9b32c92e7c88f4c188c6b8f60b4b3ed
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-13 16:00:37 +0300

    Defined route for registering user.

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = express.Router();
+const { validate, User } = require("../models/user");
+
+route.get("/", (req, res) => {
+  User.find()
+    .sort("fullName")
+    .then((users) => res.send(users));
+});
+
+route.get("/:id", async (req, res) => {
+  const user = User.findById(req.params.id);
+  if (!user) return res.status(404).send("User not found.");
+  return res.send(user);
+});
+
+route.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  const user = new User(req.body);
+  await user.save();
+  return res.send(user);
+});
