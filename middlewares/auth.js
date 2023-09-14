2bb04c8d6c738849f45748676794905b9783fb23
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-14 11:08:12 +0300

    Created middleware for protecting routes.

---
+++ middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const token = req.header("x-login-token");
+  if (!token) return res.status(401).send("Access Denied. Empty token.");
+
+  try {
+    const decoded = jwt.decode(token, process.env("JWTkey"));
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    return res.status(400).send("Invalid Token.");
+  }
+};
+
+module.exports = auth;
