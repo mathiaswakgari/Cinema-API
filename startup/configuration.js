882ad338f8c533107adc48ae8ea9c5f21eda45fb
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-15 20:45:07 +0300

    Configure environment.

--- startup/configuration.js
+++ startup/configuration.js
@@ -4,6 +4,8 @@ const logger = require("../logger");
 module.exports = function (app) {
   // configuration
   logger.log("info", `Application Name: ${config.get("name")}`);
+  console.log(`Env: ${app.get("env")}`);
+
   if (app.get("env") === "development") {
     console.log("Morgan enabled");
     app.use(morgan("tiny"));
