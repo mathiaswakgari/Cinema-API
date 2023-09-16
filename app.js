4a887c949b12bc196fd40c2696f02f8a29651747
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-16 11:39:16 +0300

    Configure db url

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+
 require("./startup/logging")();
 require("./startup/db")();
 require("./startup/configuration")(app);
