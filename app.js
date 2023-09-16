f69b6511e417174f02bb1618f39e1a88ba2ead55
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-16 11:16:14 +0300

    Perparing the app for integration testing.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+
 require("./startup/logging")();
 require("./startup/db")();
 require("./startup/configuration")(app);
