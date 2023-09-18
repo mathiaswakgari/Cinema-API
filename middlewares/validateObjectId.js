1e1ed1c9cb62329fe36c4d2691bd6d1d1cead622
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-09-18 10:12:19 +0300

    middle ware for validating object ids.

---
+++ middlewares/validateObjectId.js
@@ -0,0 +1,7 @@
+const { default: mongoose } = require("mongoose");
+
+module.exports = (req, res, next) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params._id))
+    return res.status(404).send("Invalid ID.");
+  next();
+};
